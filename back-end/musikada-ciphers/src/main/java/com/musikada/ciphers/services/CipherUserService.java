4b6efbe7d19e73c4bd9409655577124d5fbb128d
Zemiro014 <zemiro014@gmail.com>
2021-11-04 21:58:24 -0300

    Comunicação entre projectos via Feign Client

--- back-end/musikada-ciphers/src/main/java/com/musikada/ciphers/services/CipherUserService.java
+++ back-end/musikada-ciphers/src/main/java/com/musikada/ciphers/services/CipherUserService.java
@@ -1,30 +1,21 @@
 package com.musikada.ciphers.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.musikada.ciphers.cipherfeigningclients.CipherFeignClient;
 import com.musikada.ciphers.entities.Cipher;
 import com.musikada.ciphers.entities.User;
 
 @Service
 public class CipherUserService {
-	@Value("${musikada-user}")
-	private String musikadaUserHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private CipherFeignClient cipherFeignClient;
 
 	public Cipher getCipherByUserId(Long userId) {
-		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", userId.toString());
-		User user = restTemplate.getForObject(musikadaUserHost+"/users/{id}", User.class, uriVariables);
-		System.out.println(user.getFirstName()+" "+user.getEmail());
+		User user = cipherFeignClient.findById(userId).getBody();
+		System.err.println("Dados retornados: "+user.getFirstName()+" "+user.getEmail());
 		return new Cipher("Tu és a mulher", "D E");
 	}
 }
